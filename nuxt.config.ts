dd8101b6b6ef356915b62dc7ab605022608e829a
Oliver-Tobias Ripka <contact@oripka.de>
2024-04-10 20:44:07 +0200

    repo

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+
+
+export default defineNuxtConfig({
+  extends: '@nuxt/ui-pro',
+  telemetry: false,
+  modules: [
+    'nuxt-time',
+    'nuxt-gtag',
+    "@nuxtjs/supabase",
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxthq/studio',
+    '@vueuse/nuxt',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/sitemap',
+    'nuxt-schema-org',
+    '@nuxtjs/i18n',
+    '@nuxtjs/google-fonts',
+    'nuxt-icon',
+    'nuxt-og-image',
+    "@nuxt/image"
+  ]
+})
